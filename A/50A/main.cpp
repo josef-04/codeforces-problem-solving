d7d586773056e64db26585ebe6aaa4bca09969ef
kali <185556363+josef-04@users.noreply.github.com>
2025-02-24 17:34:48 +0200

    50A

---
+++ A/50A/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+ 
+    int l,w ,area=0,domino_num=0;
+ 
+    cin>>l>>w;
+    area=l*w;
+    domino_num=area/2;
+    cout<<domino_num;
+ 
+ 
+ 
+}
