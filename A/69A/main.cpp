8c02937ff816817ccbdc18c49949aaa74adbeb7b
kali <185556363+josef-04@users.noreply.github.com>
2025-03-04 20:31:41 +0200

    69A

---
+++ A/69A/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    int n,x=0,y=0,z=0;
+    cin >> n;
+    int arr[n][3];
+ 
+    for(int i=0; i<n;i++)
+    {
+        for(int j=0; j<3;j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    for(int i=0; i<n;i++)
+    {
+        x +=arr[i][0];
+        y +=arr[i][1];
+        z +=arr[i][2];
+    }
+    if(x == 0 && y == 0 && z ==0) cout << "YES";
+    else cout<< "NO";
+ 
+ 
+    return 0;
+}
